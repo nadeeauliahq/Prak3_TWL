508a597bda0a1551aa24701ff784a42ca7153cc1
Nadia <nadeeaulia@gmail.com>
2023-06-14 18:09:57 +0700

    Praktikum 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Components/style.css';
+import Meal from './Components/Meal';
+import {Route,Routes} from 'react-router-dom';
+import RecipeInfo from './Components/RecipeInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+   <Routes>
+      <Route  path="/" element={<Meal/>}/>
+      <Route exact path="/:MealId" element={<RecipeInfo/>}/>
+      </Routes>
+    </>
+  )
 }
 
 export default App;
